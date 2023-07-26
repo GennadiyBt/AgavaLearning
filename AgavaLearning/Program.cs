76dacd311e714cf2c0af9d0c2bf2c6183f9b4281
Gennadiy <safandsec71@gmail.com>
2023-07-26 12:30:42 +0300

    Добавьте файлы проекта.

---
+++ AgavaLearning/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AgavaLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            int programCount;
+            int money;
+            int time;
+            bool truth;
+
+            name = "Gennadiy";
+            age = 45;
+            programCount = 3;
+            money = 1000;
+            time = 1000000;
+            truth = true;
+
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ".");
+            Console.WriteLine("Мне " + age + " лет.");
+            Console.WriteLine("Я хочу научиться делать игры, это моя " + programCount + " программа на языке C#.");
+            Console.WriteLine("Когда я закончу обучение я хочу зарабатывать " + money + " денег в " + time + " наносекунд.");
+            Console.WriteLine("Достигну ли я желаемого? " + truth + "!");
+        }
+    }
+}
